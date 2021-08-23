aec06c1e1fcfa41f50fe6bc6cae0e1652cced364
alexdm0 <alexdemulder@gmail.com>
2021-08-23 12:02:23 +0200

    do not start server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {ApplicationConfig, ExpressServer} from './server';
 import {WebHookSystem} from "./webhookSystem/WebHookSystem";
 import {DbRef} from "./webhookSystem/DbReference";
 import {EventListenerRepository, UsageHistoryRepository, UserRepository} from "./repositories";
+import {SocketManager, SocketManager_next} from "./sockets/socket/SocketManagers";
 
 export * from './server';
 
@@ -12,27 +13,37 @@ const config = {
   },
 };
 
+if (process.env["CROWNSTONE_CLOUD_SOCKET_ENDPOINT"]) {
+  SocketManager.setCallback((event) => { WebHookSystem.dispatch(event); })
+  SocketManager.setupConnection(process.env["CROWNSTONE_CLOUD_SOCKET_ENDPOINT"] as string);
+}
+
+if (process.env["CROWNSTONE_CLOUD_NEXT_SOCKET_ENDPOINT"]) {
+  SocketManager_next.setCallback((event) => { WebHookSystem.dispatch(event); })
+  SocketManager_next.setupConnection(process.env["CROWNSTONE_CLOUD_NEXT_SOCKET_ENDPOINT"] as string);
+}
+
 export async function main(options: ApplicationConfig = {}) {
-  const server = new ExpressServer(config);
-  await server.boot();
-  await server.start();
-
-  const port = server.lbApp.restServer.config.port ?? 3000;
-  // const host = server.lbApp.restServer.config.host ?? 'NO-HOST';
-
-  console.log("Using configuration port:", port, 'options', options);
-
-  DbRef.listeners = await server.lbApp.getRepository(EventListenerRepository);
-  DbRef.usage     = await server.lbApp.getRepository(UsageHistoryRepository);
-  DbRef.user      = await server.lbApp.getRepository(UserRepository);
-
-  console.log(`Server initializing the webhook system`);
-
-  await WebHookSystem.initialize();
-
-  console.log(`Server is running at ${port}`);
-
-  return server.lbApp;
+  // const server = new ExpressServer(config);
+  // await server.boot();
+  // await server.start();
+  //
+  // const port = server.lbApp.restServer.config.port ?? 3000;
+  // // const host = server.lbApp.restServer.config.host ?? 'NO-HOST';
+  //
+  // console.log("Using configuration port:", port, 'options', options);
+  //
+  // DbRef.listeners = await server.lbApp.getRepository(EventListenerRepository);
+  // DbRef.usage     = await server.lbApp.getRepository(UsageHistoryRepository);
+  // DbRef.user      = await server.lbApp.getRepository(UserRepository);
+  //
+  // console.log(`Server initializing the webhook system`);
+  //
+  // await WebHookSystem.initialize();
+  //
+  // console.log(`Server is running at ${port}`);
+  //
+  // return server.lbApp;
 }
 
 if (require.main === module) {
